3605de20e006d1854050624b553318003d3fb39a
Balázs <arpad.balazs93@hotmail.com>
2024-08-25 13:22:08 +0200

    Számológép

---
+++ Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegatesexercise
+{
+    public class Calculator
+    {
+        public delegate double MathOperation(double x, double y);
+        public double Calculate(MathOperation mathOperation,double x,double y)
+        {
+            double művelet = mathOperation(x, y);
+            return művelet;
+        }
+        public static double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public static double Substract(double x, double y)
+        {
+            return x - y;
+        }
+        public static double Divide(double x, double y)
+        {
+            return x / y;
+        }
+        public static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,37 @@
-﻿namespace delegatesexercise;
+﻿using static delegatesexercise.Calculator;
+
+namespace delegatesexercise;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Válassza ki a műveletet:\r\n1. Összeadás\r\n2. Kivonás\r\n3. Szorzás\r\n4. Osztás");
+        Console.WriteLine("Kérem a választást:");
+        int a=int.Parse(Console.ReadLine());
+        Console.WriteLine("Kérem az első számot:");
+        double b=Double.Parse(Console.ReadLine());
+        Console.WriteLine("Kérem a második számot:");
+        double c=Double.Parse(Console.ReadLine());
+        Calculator calculator = new Calculator();
+        if (a == 1)
+        {
+            MathOperation mathOperation = Add;
+            Console.WriteLine(calculator.Calculate(mathOperation, b, c));
+        }
+        else if (a == 2)
+        {
+            MathOperation mathOperation2 = Substract;
+            Console.WriteLine(calculator.Calculate(mathOperation2, b, c));
+        }
+        else if (a == 3)
+        {
+            MathOperation mathOperation3 = Multiply;
+            Console.WriteLine(calculator.Calculate(mathOperation3, b, c));
+        }else if (a == 4)
+        {
+            MathOperation mathOperation4 = Divide;
+            Console.WriteLine(calculator.Calculate(mathOperation4, b, c));
+        }
     }
 }
